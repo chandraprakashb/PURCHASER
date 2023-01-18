406ab31f3963d77734fe0193b57cc1803f68e329
chandraprakashbagde1 <chandraprakashbagde1@gmail.com>
2023-01-18 21:36:30 +0530

    Feedback Transtion

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,7 @@ function ManageIndexHtml () {
         this.manageHelp();
         this.loadUserLinks();
         this.manageDefaulterStatus();
-        pageLoad('upcomingauction');
+        pageLoad('feedback');
     } 
 
     // Manage Render UserInfo 
